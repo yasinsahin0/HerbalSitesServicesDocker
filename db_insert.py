b6255d43fd57f6a8e4c3440f6e86a9549911088b
Yasin Şahin <56588137+yasinsahin0@users.noreply.github.com>
2022-04-16 18:51:56 +0300

    Update db_insert.py

--- db_insert.py
+++ db_insert.py
@@ -4,5 +4,6 @@ import couchdb
 couch = couchdb.Server("http://admin:admin@172.105.73.62:5984/")
 
 database = couch["products"]
-doc = {"test": "test"}
-database.save(doc)
+print(database)
+# doc = {"test": "test"}
+# database.save(doc)
